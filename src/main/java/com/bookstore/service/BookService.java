4bb8e7708ab5c5e67a37ae7f0f28fb526f326e13
Anup <anup>
2021-05-25 03:06:27 +0530

    adding service classes to perform business logic

---
+++ src/main/java/com/bookstore/service/BookService.java
@@ -0,0 +1,21 @@
+package com.bookstore.service;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+
+import com.bookstore.model.Book;
+
+import java.util.List;
+public interface BookService {
+	Book save(Book book);
+
+    void delete(Book book);
+
+    Book findOne(String id);
+
+    Iterable<Book> findAll();
+
+    Page<Book> findByAuthor(String author, PageRequest pageRequest);
+
+    List<Book> findByTitle(String title);
+}
